ecf8f6d0af9b6070525340c6625c5be1e7a33a58
Lara-Sophie <Lara-Sophie.Lohdau@Student.HTW-Berlin.de>
2023-12-28 15:42:22 +0100

    reservierungsklasse

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
+[![Java CI with Gradle](https://github.com/Lara-Sophie/Tischreservierung-Herthas-Diner-/actions/workflows/gradle.yml/badge.svg)](https://github.com/Lara-Sophie/Tischreservierung-Herthas-Diner-/actions/workflows/gradle.yml)
+
 # Tischreservierung-Herthas-Diner-
 Tischreservierung mit Anworten in Berliner Schnauze 
 Reservierungen werden gespeichert und können nachgesehen werden
 
 Teammitglieder: Paul Heinecke, Lara-Sophie Lohdau
+
--- src/main/java/com/example/demo/Tischreservierung_herthas_diner.java
+++ src/main/java/com/example/demo/Tischreservierung_herthas_diner.java
@@ -1,73 +1,68 @@
 package com.example.demo;
 
 import jakarta.persistence.*;
+import java.util.Date;
+
 
 @Entity
 public class Tischreservierung_herthas_diner {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
-        private String name;
-        private int price;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int tischnummer;
+    private date zeit;
+    private date datum;
+    private int personen;
 
-        public Tischreservierung_herthas_diner() {}
+    public Tischreservierung_herthas_diner() {}
 
-        public Tischreservierung_herthas_diner(String name, int price) {
-            this.name = name;
-            this.price = price;
-        }
-
-        public Long getId() {
-            return id;
-        }
-
-        public void setId(Long id) {
-            this.id = id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public int getPrice() {
-            return price;
-        }
-
-        public void setPrice(int price) {
-            this.price = price;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (!(o instanceof Tischreservierung_herthas_diner)) return false;
-
-            Tischreservierung_herthas_diner reservierung= (Tischreservierung_herthas_diner) o;
-
-            if (getPrice() != reservierung.getPrice()) return false;
-            if (getId() != null ? !getId().equals(reservierung.getId()) : reservierung.getId() != null) return false;
-            return getName() != null ? getName().equals(reservierung.getName()) : reservierung.getName() == null;
-        }
-
-        @Override
-        public int hashCode() {
-            int result = getId() != null ? getId().hashCode() : 0;
-            result = 31 * result + (getName() != null ? getName().hashCode() : 0);
-            result = 31 * result + getPrice();
-            return result;
-        }
-
-        @Override
-        public String toString() {
-            return "Tischreservierung_herthas_diner{" +
-                    "id=" + id +
-                    ", name='" + name + '\'' +
-                    ", price=" + price +
-                    '}';
-        }
+    public Tischreservierung_herthas_diner(int tischnummer, int id, date zeit, date datum) {
+        this.tischnummer = tischnummer;
+        this.id = id;
+        this.zeit = zeit;
+        this.datum = datum;
     }
 
+    public int getTischnummer() {return tischnummer;}
+
+    public void setTischnummer(int tischnummer) {this.tischnummer = tischnummer;}
+
+    public date getZeit() {return zeit;}
+
+    public void setZeit(date zeit) {this.zeit = zeit;}
+
+    public date getDatum() {return datum;}
+
+    public void setDatum(date datum) {this.datum = datum;}
+
+    public int getPersonen() {return personen;}
+
+    public void setPersonen(int personen) {this.personen = personen;}
+
+    public int getId() {return id;}
+
+    public void setId(int id) {this.id = id;}
+
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+        Tischreservierung_herthas_diner that = (Tischreservierung_herthas_diner) object;
+        return id == that.id && tischnummer == that.tischnummer && personen == that.personen && java.util.Objects.equals(zeit, that.zeit) && java.util.Objects.equals(datum, that.datum);
+    }
+
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id, tischnummer, zeit, datum, personen);
+    }
+
+    @Override
+    public String toString() {
+        return "Tischreservierung_herthas_diner{" +
+                "id=" + id +
+                ", Tischnummer='" + tischnummer + '\'' +
+                ", Zeit='" + zeit + '\'' +
+                ", Datum='" + datum + '\'' +
+                ", Personen=" + personen +
+                '}';
+    }
+}
