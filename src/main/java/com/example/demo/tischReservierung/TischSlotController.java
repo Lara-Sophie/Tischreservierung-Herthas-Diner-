b1598d57cc0bce93e497b12a1cbe568227453eff
HeineckePaul <bin.ich.schuld@posteo.net>
2024-01-11 23:08:28 +0100

    Docker

--- src/main/java/com/example/demo/tischReservierung/TischSlotController.java
+++ src/main/java/com/example/demo/tischReservierung/TischSlotController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/tischSlot")
+@RequestMapping("/")
 public class TischSlotController {
 
     @Autowired
